783a693d41617b610889b02f139e7109e3be27d8
Eirini Zormpa <ezormpa@turing.ac.uk>
2022-11-09 07:17:55 +0000

    updated demo after workshop

--- r-training/demos/starting-with-data.R
+++ r-training/demos/starting-with-data.R
@@ -1,18 +1,11 @@
-#  create directories
+#dir.create("data_raw")
+#dir.create("data_clean")
+#download.file(url = "https://opendata.ecdc.europa.eu/covid19/nationalcasedeath_archive/csv/data.csv",
+#destfile = "data_raw/covid-data.csv")
 
-# dir.create("data_raw")
-# dir.create("data_clean")
-# dir.create("scripts")
+#install.packages("tidyverse")
 
-# install packages
-# install.package("tidyverse)
-
-# load packages
 library(tidyverse)
-
-# download data
-download.file(url = "https://opendata.ecdc.europa.eu/covid19/nationalcasedeath_archive/csv/data.csv",
-              destfile = "data_raw/covid-data.csv")
 
 # read in data
 covid_data <- read_csv(file = "data_raw/covid-data.csv")
@@ -20,84 +13,79 @@ covid_data
 
 view(covid_data)
 
-# inspecting dataframes
-# size
+# view data size
 dim(covid_data)
 nrow(covid_data)
 ncol(covid_data)
 
-# content
-head(covid_data)
+# content of data
+head(covid_data, n = 10)
 tail(covid_data)
-
-# variable names
-names(covid_data)
 
 # summaries
 str(covid_data)
 glimpse(covid_data)
 summary(covid_data)
 
-# recap
-covid_cases <- c(0, 1, 3, 1, 5, 61)
+# indexing and subsetting
+
+covid_cases <- c(0, 1, 3, 1, 6, 51)
 covid_cases[2]
 
-# tibble[row, data]
-# one cell
+# tibble[row, column]
 covid_data[1, 1]
-covid_data[1, 6]
+covid_data[1, 2]
+covid_data[290, 1]
 
-# one column
-covid_data[1]
-
-# one row
 covid_data[1, ]
+covid_data[1]
+covid_data[5]
+covid_data[, 5]
 
-# multiple elements
-covid_data[1:6, 1:6]
-covid_data[1, 1:6]
-
-covid_data[1:6]
 covid_data[1:6, ]
 
-covid_data[-1]
+covid_data[, 1:6]
+
+covid_data[1:6, 1:6]
+
+covid_data[1]
 
 covid_data[[1]]
-covid_data$country
+covid_data$country_code
 
-# factors
-countries <- factor(c("Greece", "United Kingdom", "Germany", "Australia", "Netherlands", "Greece"))
+min_population <- min(covid_data$population)
+max(covid_data$population)
+
+# foctors
+countries <- c("United Kingdom", "Netherlands", "Greece", "Germany", "Australia")
+class(countries)
+
+countries <- factor(c("United Kingdom", "Netherlands", "Greece", "Germany", "Australia"))
+class(countries)
 
 levels(countries)
+
+countries <- factor(countries,
+                    levels = c("Greece", "United Kingdom", "Netherlands", "Australia", "Germany"))
+levels(countries)
+
 nlevels(countries)
 
-# relevelling
-countries <- factor(countries,
-                    levels = c("Greece", "United Kingdom", "Netherlands", "Germany", "Australia"))
-
-levels(countries)
-
-countries <- fct_recode(countries,
-                        `The Netherlands` = "Netherlands")
-levels(countries)
+countries <- fct_recode(countries, `The Netherlands` = "Netherlands")
 
 levels(countries)[2] <- "UK"
 levels(countries)
 
-# ordered factors
 ordered_factor <- factor(c("high", "medium", "low"),
                          levels = c("low", "medium", "high"),
                          ordered = TRUE)
 ordered_factor
 
 # converting factors
-as.character(countries)
 
-year_fct <- factor(c(1990, 1983, 1977, 1998, 1990))
-as.numeric(year_fct)
+countries_chr <- as.character(countries)
+class(countries_chr)
 
-date_data <- read_csv(file = "./data/dates.csv")
-
-str(date_data)
-library(lubridate)
-ymd(date_data$to-date)
+years <- factor(c(1456, 1987, 1887, 1654))
+years_num <- as.numeric(years)
+years_num
